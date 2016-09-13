fb3a5561afdfedd40c5cf0b912913e06292058a4
saabina <sabina.hudziak@wp.pl>
2016-09-13 19:57:03 +0300

    Login page

--- app/src/main/java/fi/jamk/basicuicontrols/MainActivity.java
+++ app/src/main/java/fi/jamk/basicuicontrols/MainActivity.java
@@ -4,8 +4,12 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AutoCompleteTextView();
     }
 
-    public void selectButtonClicked(View view) {
-        RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup);
-        int id = rg.getCheckedRadioButtonId();
-        RadioButton rb = (RadioButton)findViewById(id);
-        String text = (String)rb.getText();
-        Toast.makeText(getApplicationContext(),text,Toast.LENGTH_LONG).show();
+    public void AutoCompleteTextView() {
+        AutoCompleteTextView actv = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView_login);
+        ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,
+                new String[] {"Pasi", "Juha", "Kari", "Jouni", "Esa", "Hannu"});
+        actv.setAdapter(aa);
     }
+
+    public void onClickButtonLogin(View view) {
+        AutoCompleteTextView log = (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView_login);
+        EditText pass = (EditText)findViewById(R.id.editText_pass);
+
+        Toast.makeText(getApplicationContext(), log.getText() + " " + pass.getText(), Toast.LENGTH_LONG).show();
+    }
+
 }
